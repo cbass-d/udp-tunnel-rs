f05b22a07374ced821bb1f2bd45956451b310034
cbass-d <sebassd1.12@gmail.com>
2025-11-17 18:39:21 -0600

    basic working udp "listener" and udp socket with wirguard

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
-use anyhow::Result;
-use boringtun::noise::{Tunn, TunnResult};
-use boringtun::x25519;
+use anyhow::{Result, anyhow};
+use boringtun::noise::{Packet, Tunn, TunnResult};
+pub use boringtun::x25519;
 use log::{error, info, warn};
-use std::net::SocketAddr;
-use std::time::Duration;
-use tokio::net::UdpSocket;
-use tokio::time::timeout;
+use std::{collections::HashMap, net::SocketAddr, time::Duration};
+use tokio::{net::UdpSocket, time::timeout};
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum HandshakeState {
@@ -39,16 +37,16 @@ impl WgSocket {
         socketaddr: SocketAddr,
         peer_static_public: x25519::PublicKey,
         peer_socketaddr: SocketAddr,
-    ) -> Self {
+    ) -> Result<Self> {
         let tunn = Tunn::new(static_private, peer_static_public, None, None, index, None).unwrap();
 
-        Self {
+        Ok(Self {
             tunn,
             socketaddr,
             socket: None,
             peer_socketaddr,
             handshake: HandshakeState::None,
-        }
+        })
     }
 
     pub async fn bind(&mut self) -> Result<()> {
@@ -78,6 +76,7 @@ impl WgSocket {
         Ok(())
     }
 
+    // Perform WG handshake as initiator
     pub async fn initiate_handshake(&mut self) -> Result<()> {
         if self.socket.is_none() {
             error!("Handshake initiation failed: no binded socket");
@@ -90,28 +89,30 @@ impl WgSocket {
 
         // Create and send handshake init
         let mut dst = vec![0u8; 2048];
-        let handshake_init = self.tunn.format_handshake_initiation(&mut dst, false);
-        let handshake_init = if let TunnResult::WriteToNetwork(data) = handshake_init {
+        let handshake_init = if let TunnResult::WriteToNetwork(data) =
+            self.tunn.format_handshake_initiation(&mut dst, false)
+        {
             data
         } else {
             unreachable!();
         };
-        socket.send(handshake_init).await?;
 
+        socket.send(handshake_init).await?;
         info!("Handhsake init message sent");
 
         // Recieve and parse response
         let mut buf = vec![0u8; 2048];
-        if let Ok(Ok(len)) = timeout(Duration::from_secs(3), socket.recv(&mut buf)).await {
+        if let Ok(Ok(len)) = timeout(Duration::from_secs(10), socket.recv(&mut buf)).await {
             info!("Handshake resp message received");
 
-            let resp = self.tunn.decapsulate(None, &buf[..len], &mut dst);
+            let handshake_resp = self.tunn.decapsulate(None, &buf[..len], &mut dst);
             // Create and send keep alive
-            let keepalive = if let TunnResult::WriteToNetwork(data) = resp {
+            let keepalive = if let TunnResult::WriteToNetwork(data) = handshake_resp {
                 data
             } else {
                 unreachable!();
             };
+
             socket.send(keepalive).await?;
 
             info!("Keepalive message sent");
@@ -123,17 +124,17 @@ impl WgSocket {
         Ok(())
     }
 
+    // Perform WG handshake as responder
     pub async fn handshake_response(&mut self) -> Result<()> {
         if self.socket.is_none() {
             error!("Handshake response failed: no binded socket");
             return Err(WgSocketError::NotBinded.into());
         }
         let socket = self.socket.as_ref().unwrap();
-
-        let mut buf = vec![0u8; 2048];
-
         self.handshake = HandshakeState::Started;
+
         // Receive and parse handshake initiation
+        let mut buf = vec![0u8; 2048];
         if let Ok(Ok(len)) = timeout(Duration::from_secs(3), socket.recv(&mut buf)).await {
             info!("Handshake init message received");
 
@@ -141,9 +142,9 @@ impl WgSocket {
 
             // Create and send handshake response
             let mut dst = [0u8; 2048];
-            let handshake_resp = self.tunn.decapsulate(None, handshake_init, &mut dst);
-
-            let handshake_resp = if let TunnResult::WriteToNetwork(data) = handshake_resp {
+            let handshake_resp = if let TunnResult::WriteToNetwork(data) =
+                self.tunn.decapsulate(None, handshake_init, &mut dst)
+            {
                 data
             } else {
                 unreachable!();
@@ -162,9 +163,11 @@ impl WgSocket {
         }
 
         self.handshake = HandshakeState::Done;
+
         Ok(())
     }
 
+    // Write to WG Peer
     pub async fn write(&mut self, data: &[u8]) -> Result<usize> {
         if self.handshake != HandshakeState::Done {
             error!("Write failed: no handshake");
@@ -172,21 +175,20 @@ impl WgSocket {
         }
 
         let mut dst = vec![0u8; 2048];
-        let enc_data = self.tunn.encapsulate(data, &mut dst);
-
-        let enc_data = if let TunnResult::WriteToNetwork(data) = enc_data {
-            data
-        } else {
-            unreachable!();
-        };
+        let enc_data =
+            if let TunnResult::WriteToNetwork(data) = self.tunn.encapsulate(data, &mut dst) {
+                data
+            } else {
+                unreachable!();
+            };
 
         let socket = self.socket.as_ref().unwrap();
-
         let len = socket.send(enc_data).await?;
 
         Ok(len)
     }
 
+    // Read from WG Peer
     pub async fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         if self.handshake != HandshakeState::Done {
             error!("Write failed: no handshake");
@@ -196,8 +198,9 @@ impl WgSocket {
         let socket = self.socket.as_ref().unwrap();
         let len = socket.recv(buf).await?;
         let mut dst = vec![0u8; 2048];
-        let data = self.tunn.decapsulate(None, &buf[..len], &mut dst);
-        let data = if let TunnResult::WriteToTunnelV4(data, _src) = data {
+        let data = if let TunnResult::WriteToTunnelV4(data, _src) =
+            self.tunn.decapsulate(None, &buf[..len], &mut dst)
+        {
             data
         } else {
             unreachable!();
@@ -207,17 +210,184 @@ impl WgSocket {
         buf[..out_len].copy_from_slice(data);
         Ok(out_len)
     }
+
+    // Set static private key
+}
+
+pub type PeerConnection = (x25519::PublicKey, Tunn);
+
+pub struct WgListener {
+    socketaddr: SocketAddr,
+    socket: Option<UdpSocket>,
+    peers: HashMap<SocketAddr, PeerConnection>,
+    static_secret: x25519::StaticSecret,
+    static_pub: x25519::PublicKey,
+    index: u32,
+}
+
+impl WgListener {
+    pub fn new(
+        static_secret: x25519::StaticSecret,
+        static_pub: x25519::PublicKey,
+        socketaddr: SocketAddr,
+        index: u32,
+    ) -> Self {
+        Self {
+            socketaddr,
+            socket: None,
+            peers: HashMap::new(),
+            static_secret,
+            static_pub,
+            index,
+        }
+    }
+
+    pub async fn bind(&mut self) -> Result<()> {
+        if self.socket.is_none() {
+            let socket = UdpSocket::bind(self.socketaddr).await?;
+            self.socket = Some(socket);
+            info!("UDP socket binded to {}", self.socketaddr);
+        } else {
+            warn!("Attempted to rebind socket");
+        }
+
+        Ok(())
+    }
+
+    // As a listener we will only be acting as the responder
+    // during the WG handshake
+    pub async fn handle_handshake_init(
+        &mut self,
+        handshake_init: &[u8],
+        endpoint: SocketAddr,
+    ) -> Result<()> {
+        if handshake_init.len() != 148 {
+            return Err(anyhow!("Invalid handshake init size"));
+        }
+
+        if self.socket.is_none() {
+            error!("Handshake initiation failed: no binded socket");
+            return Err(WgSocketError::NotBinded.into());
+        }
+        let socket = self.socket.as_ref().unwrap();
+
+        let handshake_init_p = if let Packet::HandshakeInit(p) =
+            Tunn::parse_incoming_packet(handshake_init).unwrap()
+        {
+            p
+        } else {
+            return Err(anyhow!("unexpected wireguard packet type"));
+        };
+        let halfhandshake = boringtun::noise::handshake::parse_handshake_anon(
+            &self.static_secret,
+            &self.static_pub,
+            &handshake_init_p,
+        )
+        .unwrap();
+
+        let peer_static_public = x25519::PublicKey::from(halfhandshake.peer_static_public);
+
+        let mut tunn = Tunn::new(
+            self.static_secret.clone(),
+            peer_static_public,
+            None,
+            None,
+            self.index,
+            None,
+        )
+        .unwrap();
+
+        let mut dst = vec![0u8; 2048];
+        match tunn.decapsulate(None, handshake_init, &mut dst) {
+            TunnResult::WriteToNetwork(data) => {
+                socket.send_to(data, endpoint).await?;
+            }
+            TunnResult::Err(e) => {
+                println!("{e:?}");
+                return Err(anyhow!("{e:?}"));
+            }
+            _ => {
+                return Err(anyhow!("unexpted tunn result"));
+            }
+        }
+        //// Recieve and parse keepalive
+        //let mut buf = vec![0u8; 2048];
+        //match timeout(Duration::from_secs(3), socket.recv(&mut buf)).await {
+        //    Ok(Ok(len)) => {
+        //        info!("Keepalive message received");
+        //        let res = tunn.decapsulate(None, &buf[..len], &mut dst);
+        //    }
+        //    Err(e) => {
+        //        return Err(e.into());
+        //    }
+        //    _ => {
+        //        return Err(anyhow!("Recieved failed"));
+        //    }
+        //}
+
+        let peer_connection: PeerConnection = (peer_static_public, tunn);
+        self.peers.insert(endpoint, peer_connection);
+
+        Ok(())
+    }
+
+    // Listen for incoming connections and messages
+    // * If peer not known, perform WG handshake
+    // * If known, decapsulate and pass back data
+    pub async fn incoming(&mut self) -> Result<Option<Vec<u8>>> {
+        if self.socket.is_none() {
+            error!("Accept failed: no binded socket");
+            return Err(WgSocketError::NotBinded.into());
+        }
+        let socket = self.socket.as_ref().unwrap();
+
+        let mut buf = vec![0u8; 2048];
+        match socket.recv_from(&mut buf).await {
+            Ok((len, peer)) => {
+                if self.peers.contains_key(&peer) {
+                    let (_pub_key, tunn) = self.peers.get_mut(&peer).unwrap();
+
+                    let mut dst = vec![0u8; 2048];
+                    match tunn.decapsulate(None, &buf[..len], &mut dst) {
+                        TunnResult::Done => {
+                            return Ok(None);
+                        }
+                        TunnResult::WriteToNetwork(data) => {
+                            return Ok(Some(data.into()));
+                        }
+                        TunnResult::Err(e) => {
+                            return Err(anyhow!("{e:?}"));
+                        }
+                        TunnResult::WriteToTunnelV4(data, _src) => {
+                            return Ok(Some(data.into()));
+                        }
+                        _ => {
+                            return Err(anyhow!("Unexpted message"));
+                        }
+                    }
+                }
+
+                let handshake_init = &buf[..len];
+                self.handle_handshake_init(handshake_init, peer).await?;
+
+                Ok(None)
+            }
+            Err(e) => {
+                return Err(e.into());
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+    use std::net::SocketAddr;
 
     use boringtun::x25519;
     use rand_core::{OsRng, RngCore};
     use tokio::task::JoinSet;
 
-    use crate::WgSocket;
+    use crate::{WgListener, WgSocket};
 
     async fn create_socket_pair() -> (WgSocket, WgSocket) {
         let addr_a = SocketAddr::from(([127, 0, 0, 1], 6001));
@@ -231,8 +401,8 @@ mod tests {
         let public_b = x25519::PublicKey::from(&secret_b);
         let idx_b = OsRng.next_u32();
 
-        let mut wg_socket_a = WgSocket::new(secret_a, idx_a, addr_a, public_b, addr_b);
-        let mut wg_socket_b = WgSocket::new(secret_b, idx_b, addr_b, public_a, addr_a);
+        let mut wg_socket_a = WgSocket::new(secret_a, idx_a, addr_a, public_b, addr_b).unwrap();
+        let mut wg_socket_b = WgSocket::new(secret_b, idx_b, addr_b, public_a, addr_a).unwrap();
 
         assert!(wg_socket_a.bind().await.is_ok());
         assert!(wg_socket_a.connect().await.is_ok());
@@ -275,5 +445,56 @@ mod tests {
         });
 
         tasks.join_all().await;
+    }
+
+    #[tokio::test]
+    async fn listener_test() {
+        let listener_priv = x25519::StaticSecret::random_from_rng(OsRng);
+        let listener_pub = x25519::PublicKey::from(&listener_priv);
+        let listener_idx = OsRng.next_u32();
+
+        let client_priv = x25519::StaticSecret::random_from_rng(OsRng);
+        let _client_pub = x25519::PublicKey::from(&client_priv);
+        let client_idx = OsRng.next_u32();
+
+        let listener_sockaddr = SocketAddr::from(([127, 0, 0, 1], 6003));
+        let client_sockaddr = SocketAddr::from(([127, 0, 0, 1], 6004));
+
+        let udp_packet = create_udp_packet();
+        let packet_clone = udp_packet.clone();
+
+        let mut tasks = JoinSet::new();
+        let _ = tasks.spawn(async move {
+            let mut wg_listener =
+                WgListener::new(listener_priv, listener_pub, listener_sockaddr, listener_idx);
+            let _ = wg_listener.bind().await;
+
+            // First call to incoming handles the handshake init
+            assert!(wg_listener.incoming().await.is_ok());
+
+            // Second call handles the keep-alive sent after handshake
+            assert!(wg_listener.incoming().await.is_ok());
+
+            // Third call recevies the actual first packet of data
+            assert_eq!(wg_listener.incoming().await.unwrap(), Some(packet_clone));
+        });
+
+        let _ = tasks.spawn(async move {
+            let mut wg_socket = WgSocket::new(
+                client_priv,
+                client_idx,
+                client_sockaddr,
+                listener_pub,
+                listener_sockaddr,
+            )
+            .unwrap();
+
+            wg_socket.bind().await.unwrap();
+            wg_socket.connect().await.unwrap();
+            wg_socket.initiate_handshake().await.unwrap();
+            wg_socket.write(&udp_packet).await.unwrap();
+        });
+
+        let _ = tasks.join_all().await;
     }
 }
