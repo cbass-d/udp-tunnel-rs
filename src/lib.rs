1129c9fd11c4ce530602ce648bde498235b0c765
cbass-d <sebassd1.12@gmail.com>
2025-11-17 23:50:29 -0600

    added comments and reduced use '?' operator

--- src/lib.rs
+++ src/lib.rs
@@ -18,19 +18,42 @@ pub enum WgSocketError {
     Timeout,
     #[error("Socket not binded")]
     NotBinded,
+    #[error("Socket not connected")]
+    NotConnected,
     #[error("Handshake not completed")]
     NoHandshake,
 }
 
+// WgSocket handles the underlying UDP socket as well as the
+// Tunn (boringtun::noise::Tunn) structure in order to create and end-to-end
+// connection with Wireguard cryptography/encryption
 pub struct WgSocket {
+    // boringtun::noise::Tunn structure
+    // Handles anything with Wireguard
     tunn: Tunn,
+
+    // Socket address the UDP socket will be binded to
     socketaddr: SocketAddr,
+
+    // Underlying UDP socket for end-to-end connection
+    // When value is None socket is not binded yet
+    // When value is Some socket is binded
     socket: Option<UdpSocket>,
+
+    // Socket address the peer endpoint the UDP socket will be connected to
     peer_socketaddr: SocketAddr,
+
+    // State of the Wireguard handshake of the Tunn structure
     handshake: HandshakeState,
+
+    // Whether the UDP socket is connected to the peer endpoint
+    connected: bool,
 }
 
 impl WgSocket {
+    // Creates a new WgSocket and Tunn using the specified static private key, index, and peer public
+    // static key and peer endpoint socket address
+    // The value of socket (UDP socket) is initialized as None as it must be binded using .bind()
     pub fn new(
         static_private: x25519::StaticSecret,
         index: u32,
@@ -38,7 +61,8 @@ impl WgSocket {
         peer_static_public: x25519::PublicKey,
         peer_socketaddr: SocketAddr,
     ) -> Result<Self> {
-        let tunn = Tunn::new(static_private, peer_static_public, None, None, index, None).unwrap();
+        let tunn = Tunn::new(static_private, peer_static_public, None, None, index, None)
+            .expect("failed to create Tunn");
 
         Ok(Self {
             tunn,
@@ -46,12 +70,18 @@ impl WgSocket {
             socket: None,
             peer_socketaddr,
             handshake: HandshakeState::None,
+            connected: false,
         })
     }
 
+    // Binds the underlying UDP socket and makes is usable to connect to the peer endpoint
+    // as well as updating the socket value to Some
     pub async fn bind(&mut self) -> Result<()> {
         if self.socket.is_none() {
-            let socket = UdpSocket::bind(self.socketaddr).await?;
+            let socket = UdpSocket::bind(self.socketaddr)
+                .await
+                .expect("failed to bind socket");
+
             self.socket = Some(socket);
             info!("UDP socket binded to {}", self.socketaddr);
         } else {
@@ -61,9 +91,14 @@ impl WgSocket {
         Ok(())
     }
 
-    pub async fn connect(&self) -> Result<()> {
+    // Connects the underlying UDP socket to the peer endpoint if binded, fails otherwise
+    pub async fn connect(&mut self) -> Result<()> {
         if let Some(ref socket) = self.socket {
-            socket.connect(self.peer_socketaddr).await?;
+            socket
+                .connect(self.peer_socketaddr)
+                .await
+                .expect("failed to connect UDP socket");
+
             info!(
                 "Connected to peer: {} -> {}",
                 self.socketaddr, self.peer_socketaddr
@@ -73,11 +108,18 @@ impl WgSocket {
             return Err(WgSocketError::NotBinded.into());
         }
 
+        self.connected = true;
         Ok(())
     }
 
-    // Perform WG handshake as initiator
+    // Initiate the Wireguard handshake with the specified peer endpoint using the
+    // underlying Tunn structure and UDP socket. Fails if the UDP socket is not binded or connected
     pub async fn initiate_handshake(&mut self) -> Result<()> {
+        if !self.connected {
+            error!("Handshake initiation failed: socket not connected");
+            return Err(WgSocketError::NotConnected.into());
+        }
+
         if self.socket.is_none() {
             error!("Handshake initiation failed: no binded socket");
             return Err(WgSocketError::NotBinded.into());
@@ -97,7 +139,10 @@ impl WgSocket {
             unreachable!();
         };
 
-        socket.send(handshake_init).await?;
+        socket
+            .send(handshake_init)
+            .await
+            .expect("UDP socket send failed");
         info!("Handhsake init message sent");
 
         // Recieve and parse response
@@ -113,7 +158,10 @@ impl WgSocket {
                 unreachable!();
             };
 
-            socket.send(keepalive).await?;
+            socket
+                .send(keepalive)
+                .await
+                .expect("UDP socket send failed");
 
             info!("Keepalive message sent");
         } else {
@@ -124,13 +172,20 @@ impl WgSocket {
         Ok(())
     }
 
-    // Perform WG handshake as responder
+    // Respond to the Wireguard handshake initiation message.
+    // Fails if the underlyint UDP socket is not binded or connected.
     pub async fn handshake_response(&mut self) -> Result<()> {
+        if !self.connected {
+            error!("Handshake initiation failed: socket not connected");
+            return Err(WgSocketError::NotConnected.into());
+        }
+
         if self.socket.is_none() {
             error!("Handshake response failed: no binded socket");
             return Err(WgSocketError::NotBinded.into());
         }
         let socket = self.socket.as_ref().unwrap();
+
         self.handshake = HandshakeState::Started;
 
         // Receive and parse handshake initiation
@@ -150,7 +205,10 @@ impl WgSocket {
                 unreachable!();
             };
 
-            socket.send(handshake_resp).await?;
+            socket
+                .send(handshake_resp)
+                .await
+                .expect("UDP socket send failed");
 
             info!("Handshake message sent");
 
@@ -167,7 +225,9 @@ impl WgSocket {
         Ok(())
     }
 
-    // Write to WG Peer
+    // Write to the Wireguard peer by using to underlying Tunn structure to
+    // encrypt/encapsulate the data and the UDP socket to send it to the peer endpoint.
+    // Fails if the Tunn has not performed the necessary Wireguard handshake.
     pub async fn write(&mut self, data: &[u8]) -> Result<usize> {
         if self.handshake != HandshakeState::Done {
             error!("Write failed: no handshake");
@@ -183,12 +243,14 @@ impl WgSocket {
             };
 
         let socket = self.socket.as_ref().unwrap();
-        let len = socket.send(enc_data).await?;
+        let len = socket.send(enc_data).await.expect("UDP socket send failed");
 
         Ok(len)
     }
 
-    // Read from WG Peer
+    // Read from the Wireguard peer by using to underlying Tunn structure to
+    // decryp/decapsulate the data received from the UDP socket connection.
+    // Fails if the Tunn has not performed the necessary Wireguard handshake.
     pub async fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         if self.handshake != HandshakeState::Done {
             error!("Write failed: no handshake");
@@ -196,7 +258,10 @@ impl WgSocket {
         }
 
         let socket = self.socket.as_ref().unwrap();
-        let len = socket.recv(buf).await?;
+        let len = socket
+            .recv(buf)
+            .await
+            .expect("UDP socket recv calle failed");
         let mut dst = vec![0u8; 2048];
         let data = if let TunnResult::WriteToTunnelV4(data, _src) =
             self.tunn.decapsulate(None, &buf[..len], &mut dst)
@@ -210,22 +275,41 @@ impl WgSocket {
         buf[..out_len].copy_from_slice(data);
         Ok(out_len)
     }
-
-    // Set static private key
 }
 
 pub type PeerConnection = (x25519::PublicKey, Tunn);
 
+// A listener that uses a UDP socket to handle one-to-many connections
+// that uses a dedictaed Tunn (boringtun::noise::Tunn) for each of the
+// connections for an end-to-end connection with Wireguard cryptography/encryption
 pub struct WgListener {
+    // Socket address the listener is binded to
     socketaddr: SocketAddr,
+
+    // Underlying UDP socket for end-to-end connections
+    // When value is None socket is not binded yet
+    // When value is Some socket is binded
     socket: Option<UdpSocket>,
+
+    // Hashmap containing the active connected peers.
+    // Indexed by the peer endpoint socket address and points
+    // to the peer public key and respective Tunn structure
     peers: HashMap<SocketAddr, PeerConnection>,
+
+    // Static secret used to build the needed Tunn structures for
+    // the client connections
     static_secret: x25519::StaticSecret,
+
+    // Static public key
     static_pub: x25519::PublicKey,
+
+    // Index used in Wireguard protocol
     index: u32,
 }
 
 impl WgListener {
+    // Creates a new WgListener using the specified static private key, index, and peer public
+    // static key and peer endpoint socket address
     pub fn new(
         static_secret: x25519::StaticSecret,
         static_pub: x25519::PublicKey,
@@ -242,9 +326,14 @@ impl WgListener {
         }
     }
 
+    // Binds the underlying UDP socket and makes is usable for future use,
+    // as well as updating the socket value to Some
     pub async fn bind(&mut self) -> Result<()> {
         if self.socket.is_none() {
-            let socket = UdpSocket::bind(self.socketaddr).await?;
+            let socket = UdpSocket::bind(self.socketaddr)
+                .await
+                .expect("failed to bind UDP socket");
+
             self.socket = Some(socket);
             info!("UDP socket binded to {}", self.socketaddr);
         } else {
@@ -254,8 +343,10 @@ impl WgListener {
         Ok(())
     }
 
-    // As a listener we will only be acting as the responder
-    // during the WG handshake
+    // Handles and responds to the Wireguard handshake initiation
+    // message recieved from an incoming connection. Once handshake succeeds
+    // the connection is added as a peer and the corresponding Tunn structure
+    // its stored for future use.
     pub async fn handle_handshake_init(
         &mut self,
         handshake_init: &[u8],
@@ -295,12 +386,15 @@ impl WgListener {
             self.index,
             None,
         )
-        .unwrap();
+        .expect("failed to create Tunn structure for new peer");
 
         let mut dst = vec![0u8; 2048];
         match tunn.decapsulate(None, handshake_init, &mut dst) {
             TunnResult::WriteToNetwork(data) => {
-                socket.send_to(data, endpoint).await?;
+                socket
+                    .send_to(data, endpoint)
+                    .await
+                    .expect("UDP socket send_to call failed");
             }
             TunnResult::Err(e) => {
                 println!("{e:?}");
@@ -331,9 +425,10 @@ impl WgListener {
         Ok(())
     }
 
-    // Listen for incoming connections and messages
-    // * If peer not known, perform WG handshake
-    // * If known, decapsulate and pass back data
+    // Listen for incoming connections as well as messages. When a message
+    // is recieved it checks if the peer is known. If it is known we perform
+    // the decryption/decapsulation using the peer's respective Tunn structure.
+    // If it is not known, the Wireguard handshake is performed.
     pub async fn incoming(&mut self) -> Result<Option<Vec<u8>>> {
         if self.socket.is_none() {
             error!("Accept failed: no binded socket");
@@ -372,9 +467,7 @@ impl WgListener {
 
                 Ok(None)
             }
-            Err(e) => {
-                return Err(e.into());
-            }
+            Err(e) => Err(e.into()),
         }
     }
 }
